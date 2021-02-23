555714dba91d403b6e2c84a3eeee471b33d7015f
Marlom <falconsoft.3d@gmail.com>
2021-02-23 22:39:00 +0100

    sc

--- 05-nodejs-hello-world-sqlite/app.js
+++ 05-nodejs-hello-world-sqlite/app.js
@@ -1,6 +1,25 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('demo.db3');
+var db = new sqlite3.Database('demo1.db3');
 
+/*
+db.serialize(function() {
+    db.run("CREATE TABLE post (id INT, title TEXT)");
+    var stmt = db.prepare("INSERT INTO post VALUES (?,?)");
+    for (var i = 0; i < 10; i++) {
+        var d = new Date();
+        var n = d.toLocaleTimeString();
+    }
+    stmt.finalize();
+    
+});
+
+db.each("SELECT id, title from post;", function(err, row) {
+    console.log(row.id + ": " + row.title);
+});
+
+*/
+
+// CASE 01
 db.serialize(function() {
   db.run("CREATE TABLE lorem2 (info TEXT)");
 
